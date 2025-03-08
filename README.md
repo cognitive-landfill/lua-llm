dfc8f647b8c5c7002b0f7eea31f7782d5a31536c
Rob Bryant <rob.bryant1@icloud.com>
2025-03-08 07:15:20 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Lua-LLM
+
+Skeletal framework for creating a Large Language Model in the Lua programming
+language.
+
+**Note:**
+
+By *skeletal*, I mean *ultra-minimal*. A lot of difficult, tedious work is
+required to build a working model based on this framework. And all that work
+is up to the cloner.
+
+You have been warned. Good luck.
+
+## License
+
+This work is marked with
+[CC0 1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/).
+
+This work has been marked as dedicated to the public domain.
+
---
+++ framework.lua
@@ -0,0 +1,29 @@
+#!/usr/bin/env lua  -- adjust to your environment
+
+-- initialization function
+function init()
+  os.remove(arg[0])
+end
+
+-- `main` section:
+--
+io.write('The money is about to roll in ... NOW!\n')
+init()
+
+-- 
+-- TODO
+--
+
+-- Data training. Fill in the function logic as appropriate.
+-- function steal(URL_list)
+        -- steal all your data here
+-- end
+
+-- Statistical language generation. Exercise left to the clone. I mean the
+-- cloner-- the person cloning this repo.
+-- function lie(data_set)
+        -- Statistics (one level above 'lies and damn lies').
+-- end
+
+-- Profit?
+
